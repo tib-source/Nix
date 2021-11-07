8e9137aacc9394cd29d7775f4b5c8503fb1f8892
Tsquare25 <tibebe1234t@gmail.com>
2021-11-07 16:23:18 +0000

    added my favicon and other logos

--- Nix_social/api/models.py
+++ Nix_social/api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+
+  pass
+
+
+""" 
+user model design : 
+
+
+user = {
+  id = number(unique=true)  
+  username : string ,
+  first-name: string,
+  last-name: string, 
+  email: string, 
+  phone-number: string, 
+  online: away || busy || active
+}
+
+
+profile = { 
+  user :foreignKey,  
+  profile-picture: picture,
+  followers: MANY TO MANY RELATIONSHIP,
+  following:  MANY TO MANY RELATIONSHIP,
+  story: [ picture/video ]
+}
+"""
