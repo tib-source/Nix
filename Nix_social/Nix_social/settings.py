5deadfd97ab60b825cfabaf9fbe16ecc5fa7ea17
Tsquare25 <tibebe1234t@gmail.com>
2021-11-13 18:02:35 +0000

    new and hidden django secret key

--- Nix_social/Nix_social/settings.py
+++ Nix_social/Nix_social/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!9m)b-h8#=toiqm1d)-2%jc19c56+ld*f!3ue^=pofnxjz9bvb'
+SECRET_KEY = os.environ['nix']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
