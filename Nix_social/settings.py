db8b4091181ba2721dfdb88692640b18881b7e00
Tsquare25 <tibebe1234t@gmail.com>
2021-11-13 18:31:18 +0000

    allowed localhost

--- Nix_social/settings.py
+++ Nix_social/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = os.environ['nix']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+  'localhost',
+  '127.0.0.1'
+  ]
 
 
 # Application definition
