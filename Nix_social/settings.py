4d932fade71b833fae02c869d719eb64d8863094
Tsquare25 <tibebe1234t@gmail.com>
2021-11-13 19:51:27 +0000

    heroku database error fixed

--- Nix_social/settings.py
+++ Nix_social/settings.py
@@ -158,3 +158,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
