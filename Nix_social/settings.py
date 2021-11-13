89fcc5bb9b71f66d1d9ecc156abf22db0f28efa7
Tsquare25 <tibebe1234t@gmail.com>
2021-11-13 19:37:38 +0000

    more static config

--- Nix_social/settings.py
+++ Nix_social/settings.py
@@ -150,7 +150,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'nix_frontend/static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
