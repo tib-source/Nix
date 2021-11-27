1dfb280aeec57d75d20c95e853a5d9e7f46653aa
Tsquare25 <tibebe1234t@gmail.com>
2021-11-27 18:06:03 +0000

    create the profile database model

--- Nix_social/settings.py
+++ Nix_social/settings.py
@@ -155,6 +155,10 @@ STATICFILES_DIRS = (
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- api/models.py
+++ api/models.py
@@ -2,23 +2,27 @@ from django.db import models
 from django.contrib.auth import get_user_model
 # Create your models here.
 
-User = get_user_model() 
-# onlineChoice = [
-#   ("o", "online"),
-#   ("b", "busy"),
-#   ("a", "away")
-# ]
+User = get_user_model()
+onlineChoice = [
+    ("o", "online"),
+    ("b", "busy"),
+    ("a", "away")
+]
 
-class Profile(models.Model): 
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  created_at = models.DateTimeField(auto_now_add=True)
-  user_bio = models.TextField(blank=True)
-  # profile_picture= models.ImageField(upload_to='profile_picures')
-  # followers = models.ManyToManyField(User)
-  # following = models.ManyToManyField(User)
-  # story = models.ImageField(upload_to='stories') 
-  # online = models.CharField(max_length=10, choices=onlineChoice, default='online')
 
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user_bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(
+        default='default.jpg', upload_to='profile_picures')
+    followers = models.ManyToManyField(User, related_name="followers")
+    following = models.ManyToManyField(User, related_name="following")
+    story = models.ImageField(upload_to='stories', null=True)
+    online = models.CharField(
+        max_length=10, choices=onlineChoice, default='online')
 
 
 """ 
