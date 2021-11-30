91bdf2daf4eb19ff7f19538c3584e20c135e1270
Tsquare25 <tibebe1234t@gmail.com>
2021-11-30 23:40:16 +0000

    created serializers for the models

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
+from django.db.models import fields
 from rest_framework import serializers
-from .models import Profile
+from .models import Comment, Likes, Post, Profile
 
 
 class profileSerializer(serializers.ModelSerializer):
@@ -7,3 +8,21 @@ class profileSerializer(serializers.ModelSerializer):
         model = Profile
         read_only_fields = ('id', 'created_at', 'user_bio', 'user')
         fields = ('id', 'created_at', 'user_bio', 'user')
+
+
+class postSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = "__all__"
+
+
+class likesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Likes
+        fields = "__all__"
+
+
+class commentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = "__all__"
